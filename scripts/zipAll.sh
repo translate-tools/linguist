9d1cf20ef6a05af55edfee32aab93f15352dfd18
vitonsky <86191922+vitonsky@users.noreply.github.com>
2021-08-01 14:40:31 +0300

    build(zipAll): improve description

--- scripts/zipAll.sh
+++ scripts/zipAll.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/sh
 
+# Script make zip files for all directories from the current directory
+
+# TODO: exclude dev directories
 for buildDir in `find -maxdepth 1 -type d -not -name . -print`;
 do
 	zip "$(basename $buildDir).zip" "$buildDir"/*;
