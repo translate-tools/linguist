f12af95dc96019c45a0ffefc032e0a5c9f7be9a8
vitonsky <86191922+vitonsky@users.noreply.github.com>
2021-08-12 16:34:49 +0300

    chore: add hook `useStateWithProxy`

---
+++ src/lib/hooks/useStateWithProxy.ts
@@ -0,0 +1,32 @@
+import { Dispatch, SetStateAction, DependencyList, useState } from 'react';
+import { useImmutableCallback } from 'react-elegant-ui/esm/hooks/useImmutableCallback';
+
+/**
+ * Like `useState` but with proxy
+ *
+ * Return [value, setterWithProxy, originalSetter]
+ */
+export const useStateWithProxy = <S>(
+	initialState: S | (() => S),
+	proxy?: (value: React.SetStateAction<S>, setter: Dispatch<SetStateAction<S>>) => void,
+	deps?: DependencyList,
+): [S, Dispatch<SetStateAction<S>>, Dispatch<SetStateAction<S>>] => {
+	const [value, setter] = useState<S>(initialState);
+
+	// Define proxy
+	const proxySetter: Dispatch<SetStateAction<S>> = useImmutableCallback(
+		(newValue) => {
+			if (proxy !== undefined) {
+				proxy(newValue, setter);
+			} else {
+				setter(newValue);
+			}
+		},
+
+		// Ignore spread
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[proxy, ...(deps ?? [])],
+	);
+
+	return [value, proxySetter, setter];
+};
