bcfb83aa28f42f85448c34bd2bf47710e09e6aa0
vitonsky <86191922+vitonsky@users.noreply.github.com>
2021-08-14 17:32:21 +0300

    feat: add `AbstractVersionedStorage`

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -32,3 +32,25 @@ export type RecordValues<T extends Record<any, any>> = {
  * Make type from object values
  */
 export type RecordValue<T extends Record<any, string | number>> = keyof RecordValues<T>;
+
+/**
+ * Abstract storage object.
+ *
+ * All storages must extend this class to standardize behavior
+ */
+export abstract class AbstractVersionedStorage {
+	/**
+	 * Storage version must be increase with every change of storage structure.
+	 *
+	 * While run application this version will compared to version from storages database and if it different, will call method `updateStorageVersion`
+	 */
+	public static storageVersion = 0;
+
+	/**
+	 * Method which call for each update storage version
+	 *
+	 * This method may be used to implement migration data between application versions,
+	 * such as migrations must run only by condition
+	 */
+	public static async updateStorageVersion(_prevVersion: number | null) {}
+}
