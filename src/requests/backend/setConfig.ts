98ba85aadec72ef0ee3bfef015e4af5f77859e51
vitonsky <86191922+vitonsky@users.noreply.github.com>
2021-08-21 11:35:27 +0300

    refactor(requests): use builder for `setConfig`

--- src/requests/backend/setConfig.ts
+++ src/requests/backend/setConfig.ts
@@ -1,16 +1,11 @@
-import { addRequestHandler, bgSendRequest } from '../../lib/communication';
-import { tryDecode } from '../../lib/types';
-import { AppConfig, AppConfigType } from '../../types/runtime';
-import { RequestHandlerFactory } from '../types';
+import { buildBackendRequest } from '../../lib/requestBuilder';
+import { AppConfig } from '../../types/runtime';
 
-export const setConfigIn = AppConfig;
-
-export const setConfig = (config: AppConfigType): Promise<void> =>
-	bgSendRequest('setConfig', config);
-
-export const setConfigFactory: RequestHandlerFactory = ({ cfg }) => {
-	addRequestHandler('setConfig', async (rawData) => {
-		const newConfig = tryDecode(setConfigIn, rawData);
-		return cfg.set(newConfig);
-	});
-};
+export const [setConfigFactory, setConfig] = buildBackendRequest('setConfig', {
+	requestValidator: AppConfig,
+	factoryHandler:
+		({ cfg }) =>
+			async (newConfig) => {
+				return cfg.set(newConfig);
+			},
+});
