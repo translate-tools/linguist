eac0bbc3e442dc89eb3c5b393a3a6e1a98409e45
vitonsky <86191922+vitonsky@users.noreply.github.com>
2021-08-22 11:15:21 +0300

    refactor(requests): use builder for `getPageLanguage`

--- src/requests/contentscript/getPageLanguage.ts
+++ src/requests/contentscript/getPageLanguage.ts
@@ -1,18 +1,21 @@
-import { ClientRequestHandlerFactory } from '../types';
-
 import { getPageLanguage as getPageLanguageHelper } from '../../lib/browser';
-import { addRequestHandler, csSendRequest } from '../../lib/communication';
-import { tryDecode, type } from '../../lib/types';
+import { type } from '../../lib/types';
+import { buildTabRequest } from '../../lib/requestBuilder';
 
-export const getPageLanguageOut = type.union([type.string, type.null]);
-
-export const getPageLanguage = (tabId: number) =>
-	csSendRequest(tabId, 'getPageLanguage').then((language) =>
-		tryDecode(getPageLanguageOut, language),
-	);
-
-export const getPageLanguageFactory: ClientRequestHandlerFactory = ({ config }) => {
-	addRequestHandler('getPageLanguage', () =>
-		getPageLanguageHelper(config.pageTranslator.detectLanguageByContent, true),
-	);
-};
+export const [getPageLanguageFactory, getPageLanguage] = buildTabRequest(
+	'getPageLanguage',
+	{
+		responseValidator: type.union([type.string, type.null]),
+		factoryHandler:
+			({ config }) =>
+				async () =>
+					getPageLanguageHelper(
+						config.pageTranslator.detectLanguageByContent,
+						true,
+					).then(
+						(rsp) =>
+						// FIXME: fix types and remove cast
+						rsp as any,
+					),
+	},
+);
