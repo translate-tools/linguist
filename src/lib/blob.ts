0cb5e8e50566df543b94bb4cb2683b7da6dd41ef
vitonsky <86191922+vitonsky@users.noreply.github.com>
2021-08-31 21:49:10 +0300

    refactor(getTTS): serialize data as base64 strings

---
+++ src/lib/blob.ts
@@ -0,0 +1,33 @@
+export const blobToBase64 = (blob: Blob) => {
+	return new Promise<string>((resolve) => {
+		const reader = new FileReader();
+		reader.onloadend = () => {
+			if (typeof reader.result === 'string') {
+				resolve(reader.result as string);
+			} else {
+				throw new TypeError('Unexpected type of result');
+			}
+		};
+		reader.readAsDataURL(blob);
+	});
+};
+
+export const base64ToBlob = (b64Data: string, contentType = '', sliceSize = 512) => {
+	const byteCharacters = atob(b64Data);
+	const byteArrays = [];
+
+	for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+		const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+		const byteNumbers = new Array(slice.length);
+		for (let i = 0; i < slice.length; i++) {
+			byteNumbers[i] = slice.charCodeAt(i);
+		}
+
+		const byteArray = new Uint8Array(byteNumbers);
+		byteArrays.push(byteArray);
+	}
+
+	const blob = new Blob(byteArrays, { type: contentType });
+	return blob;
+};
--- src/requests/backend/getTTS.ts
+++ src/requests/backend/getTTS.ts
@@ -1,13 +1,12 @@
 import splitLongText from 'google-tts-api/dist/splitLongText';
 
 import { detectLanguage } from '../../lib/language';
+import { blobToBase64, base64ToBlob } from '../../lib/blob';
 import { buildBackendRequest } from '../utils/requestBuilder';
 
 // TODO: move getting TTS blob to standalone class
-// TODO: remove objects which is unnecessary more with command `window.URL.revokeObjectURL`
-// TODO: add TTS for select translator and for dictionary
 // TODO: implement option for select TTS speed
-export const [getTTSFactory, getTTS] = buildBackendRequest('getTTS', {
+export const [getTTSFactory, getTTSReq] = buildBackendRequest('getTTS', {
 	factoryHandler:
 		() =>
 			async ({ text, lang }: { text: string; lang: string }) => {
@@ -16,18 +15,36 @@ export const [getTTSFactory, getTTS] = buildBackendRequest('getTTS', {
 					lang = (await detectLanguage(text, true)) || 'en';
 				}
 
-				const ttsBlobs = await Promise.all(
+				const ttsEncodedBlobs = await Promise.all(
 					splitLongText(text).map((text) => {
 						const url =
 						`https://translate.google.com/translate_tts?ie=UTF-8&tl=${lang}&client=dict-chrome-ex&ttsspeed=0.5&q=` +
 						encodeURIComponent(text);
 
-						return fetch(url).then((rsp) => rsp.blob());
+						return fetch(url)
+							.then((rsp) => rsp.blob())
+							.then(blobToBase64);
 					}),
 				);
 
-				const ttsUrls = ttsBlobs.map((blob) => window.URL.createObjectURL(blob));
-
-				return ttsUrls;
+				return ttsEncodedBlobs;
 			},
 });
+
+/**
+ * Wrapper on `getTTSReq` which decode base64 strings to blobs and make blob urls
+ *
+ * This architecture is one which work
+ * - We must serialize data between requests
+ * - We must use encode/decode instead blob urls, due to CORS limitations for content script
+ */
+export const getTTS = async (options: { text: string; lang: string }) => {
+	const encodedBlobs = await getTTSReq(options);
+
+	const ttsBlobs = encodedBlobs.map((encodedBlob) =>
+		base64ToBlob(encodedBlob.slice('data:audio/mpeg;base64,'.length), 'audio/mpeg'),
+	);
+
+	const ttsUrls = ttsBlobs.map((blob) => window.URL.createObjectURL(blob));
+	return ttsUrls;
+};
