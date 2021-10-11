ad5de94f6fd622a04902f4a8e90c1ad6f9d1f225
vitonsky <86191922+vitonsky@users.noreply.github.com>
2021-10-11 22:40:12 +0300

    build: add script for generate update manifest for chromium

---
+++ scripts/genCrxUpdateManifest.js
@@ -0,0 +1,16 @@
+const { version } = require(__dirname + '/../package.json');
+
+const appid = 'bjbijcdiadijepllfddlcbehcdhcfhkh';
+const codebase = `https://github.com/translate-tools/linguist/releases/download/v${version}/linguist.crx`;
+
+// Generate XML file
+// For more info see https://developer.chrome.com/docs/apps/autoupdate/#update_manifest
+const result = `<?xml version='1.0' encoding='UTF-8'?>
+<gupdate xmlns='http://www.google.com/update2/response' protocol='2.0'>
+  <app appid='${appid}'>
+    <updatecheck codebase='${codebase}' version='${version}' />
+  </app>
+</gupdate>`;
+
+// Output
+console.log(result);
