b4447f2d49c6b3beefcc9a5573c1187eeebfdf00
vitonsky <86191922+vitonsky@users.noreply.github.com>
2021-11-29 02:00:59 +0300

    build: disable build linting due to bug

--- scripts/testBuildArchives.sh
+++ scripts/testBuildArchives.sh
@@ -14,13 +14,17 @@ do
 
 	echo;
 
+	# At this time linting is disabled due to BUG https://github.com/mozilla/web-ext/issues/2245
+	# If it will not fixed, we have to replace test tool
+	# TODO: make this step optional and report about falls to TG/elsewhere by HTTP request with API key
+
 	# Check builds on errors
-	echo "Lint build \"$packageArchive\"";
-	npx web-ext lint --source-dir "$unpackDir";
-	if [ $? -ne 0 ];
-	then
-		exit 1;
-	fi;
+	# echo "Lint build \"$packageArchive\"";
+	# npx web-ext lint --source-dir "$unpackDir";
+	# if [ $? -ne 0 ];
+	# then
+	# 	exit 1;
+	# fi;
 
 	echo;
 done;
