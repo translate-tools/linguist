30b84e16426116673ed6ad59b25706ad25725701
vitonsky <86191922+vitonsky@users.noreply.github.com>
2022-03-23 01:58:08 +0300

    docs(useIsFirstRenderRef): add usage info

--- src/lib/hooks/useIsFirstRenderRef.ts
+++ src/lib/hooks/useIsFirstRenderRef.ts
@@ -1,5 +1,9 @@
 import { useRef } from 'react';
 
+/**
+ * Use for cases, when you need to use dependencies.
+ * For example, to skip some actions while first render in complexity `useEffect`
+ */
 export const useIsFirstRenderRef = () => {
 	const response = useRef(true);
 
