4b44a55a4c463e45e02fb8a160b3b337ba280f73
vitonsky <86191922+vitonsky@users.noreply.github.com>
2022-03-24 02:41:46 +0300

    refactor(TTSPlayer): load all data by start play

--- src/lib/TTSPlayer.ts
+++ src/lib/TTSPlayer.ts
@@ -3,40 +3,62 @@ import { QueuePlayer } from './QueuePlayer';
 export type UrlGetter = (options: { text: string; lang: string }) => Promise<string[]>;
 
 export class TTSPlayer {
+	private player = new QueuePlayer([]);
+
 	private getTTS: UrlGetter;
 	constructor(ttsGetter: UrlGetter) {
 		this.getTTS = ttsGetter;
 
-		this.player.onLoading = (state) => {
-			console.warn('IS LOADING', state);
-
-			this.isLoading = state;
-			if (this.onLoading !== null) {
-				this.onLoading(state);
-			}
-		};
+		this.player.onLoading = this.setLoadingState;
 	}
 
-	private player = new QueuePlayer([]);
-
-	private isLoading = false;
 	public getIsLoading = () => this.isLoading;
 	public onLoading: ((status: boolean) => void) | null = null;
 
+	private isLoading = false;
+	private setLoadingState = (state: boolean) => {
+		console.warn('IS LOADING', state);
+
+		this.isLoading = state;
+		if (this.onLoading !== null) {
+			this.onLoading(state);
+		}
+	};
+
+	private options: { lang: string; text: string | null } | null = null;
 	private ttsUrls: Promise<string[]> | null = null;
 	public setOptions = (lang: string, text: string | null) => {
+		// Remove TTS urls for previous state
+		if (
+			this.options !== null &&
+			this.options.lang !== lang &&
+			this.options.text !== text
+		) {
+			this.ttsUrls = null;
+			this.playerContext = {};
+		}
+
 		this.player.stop();
-		this.playerContext = {};
-		this.ttsUrls = text === null ? Promise.resolve([]) : this.getTTS({ lang, text });
+		this.options = { lang, text };
 	};
 
 	private playerContext = {};
 	public play = async () => {
-		if (this.ttsUrls === null) {
+		if (this.options === null) {
 			throw new Error('Player is not configured');
 		}
 
+		const { lang, text } = this.options;
+
+		if (text === null) return;
+
 		const localContext = this.playerContext;
+
+		// Load urls
+		if (this.ttsUrls === null) {
+			this.ttsUrls = this.getTTS({ lang, text });
+			this.setLoadingState(true);
+		}
 
 		const urls = await this.ttsUrls;
 
@@ -48,6 +70,17 @@ export class TTSPlayer {
 		this.player.play();
 	};
 
-	public stop = this.player.stop;
+	public toggle = () => {
+		if (this.isPlayed()) {
+			this.stop();
+		} else {
+			this.play();
+		}
+	};
+
+	public stop = () => {
+		this.player.stop();
+		this.setLoadingState(false);
+	};
 	public isPlayed = this.player.isPlayed;
 }
