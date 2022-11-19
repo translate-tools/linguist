17c9eb81492f487d99893ad905dbb6b6445e7623
vitonsky <86191922+vitonsky@users.noreply.github.com>
2022-11-19 17:12:07 +0300

    test(#155): configure fast tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,19 @@
+const jestGlobals = {};
+
+// Speedup test
+if (process.env.TEST_FAST) {
+	const RED_COLOR = '\x1b[31m';
+	console.warn(RED_COLOR, 'TEST DO NOT CHECK A TYPES!\n\n');
+
+	jestGlobals['ts-jest'] = {
+		isolatedModules: true,
+	};
+}
+
 module.exports = {
 	testEnvironment: 'node',
+
+	globals: Object.keys(jestGlobals).length === 0 ? undefined : jestGlobals,
 
 	preset: 'ts-jest/presets/js-with-ts-esm',
 
