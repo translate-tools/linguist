1f27bcfb345747a1b811f2cb5d778780fd09948a
vitonsky <86191922+vitonsky@users.noreply.github.com>
2022-11-20 21:01:40 +0300

    chore(#155): add migrations for TextTranslatorStorage

--- src/layouts/TextTranslator/TextTranslator.utils/TextTranslatorStorage.ts
+++ src/layouts/TextTranslator/TextTranslator.utils/TextTranslatorStorage.ts
@@ -1,16 +1,15 @@
 import { TypeOf } from 'io-ts';
 import { browser } from 'webextension-polyfill-ts';
 
-import { tryDecode, type } from '../../../lib/types';
+import { decodeStruct, tryDecode, type } from '../../../lib/types';
 import { LangCodeWithAuto, LangCode } from '../../../types/runtime';
 import { AbstractVersionedStorage } from '../../../types/utils';
 
 // TODO: #refactor use not static class. To implement singleton we may export instance of class
-// TODO: add tests for this storage
-// TODO: migrate data
+// TODO: add tests for this storage and for migrations
 export class TextTranslatorStorage extends AbstractVersionedStorage {
 	static publicName = 'TextTranslatorStorage';
-	static storageVersion = 2;
+	static storageVersion = 3;
 
 	public static readonly storeName = 'TextTranslatorStorage';
 	public static readonly storageSignature = type.union([
@@ -89,21 +88,75 @@ export class TextTranslatorStorage extends AbstractVersionedStorage {
 		}
 	};
 
-	public static async updateStorageVersion() {
-		const lastState = localStorage.getItem('TextTranslator.lastState');
+	// TODO: wrap it to tool for migrations. It may be a data transforming pipeline or array with cases to execution for complex migrations
+	public static async updateStorageVersion(prevVersion: number | null) {
+		const storeName = TextTranslatorStorage.storeName;
 
-		// Skip
-		if (lastState === null) return;
+		const dataStructureVersions = {
+			0: type.union([
+				type.type({
+					from: LangCodeWithAuto,
+					to: LangCode,
+					translate: type.union([
+						type.type({
+							text: type.string,
+							translate: type.union([type.string, type.null]),
+						}),
+						type.null,
+					]),
+				}),
+				type.null,
+			]),
+		};
 
-		// Try decode and set data
-		try {
-			const decodedData = JSON.parse(lastState);
-			await TextTranslatorStorage.setData(decodedData);
-		} catch (error) {
-			// Do nothing, because invalid data here it is not our responsibility domain
+		// We fall trough cases to migrate from older versions to newer
+		switch (prevVersion) {
+			case 1: {
+				const lastState = localStorage.getItem('TextTranslator.lastState');
+
+				// Skip
+				if (lastState === null) return;
+
+				// Try decode and write data to a new storage
+				try {
+					const parsedData = JSON.parse(lastState);
+					const codec = decodeStruct(dataStructureVersions[0], parsedData);
+
+					if (codec.errors === null && codec.data !== null) {
+						await browser.storage.local.set({ [storeName]: codec.data });
+					}
+				} catch (error) {
+					// Do nothing, because invalid data here it is not our responsibility domain
+				}
+
+				// Clear data
+				localStorage.removeItem('TextTranslator.lastState');
+			}
+
+			case 2: {
+				const { [storeName]: tabData } = await browser.storage.local.get(
+					storeName,
+				);
+
+				const codec = decodeStruct(dataStructureVersions[0], tabData);
+
+				// Skip invalid data
+				if (codec.errors !== null || codec.data === null) return;
+
+				const { from, to, translate } = codec.data;
+				await browser.storage.local.set({
+					[storeName]: {
+						from,
+						to,
+						translate: translate
+							? {
+								originalText: translate.text,
+								translatedText: translate.translate,
+							  }
+							: null,
+					},
+				});
+			}
 		}
-
-		// Clear data
-		localStorage.removeItem('TextTranslator.lastState');
 	}
 }
