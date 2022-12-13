e8c7c909d85625e58c344044653cc319b3a7879a
vitonsky <86191922+vitonsky@users.noreply.github.com>
2022-12-14 02:28:59 +0300

    test(#181): add tests for config migrations

---
+++ src/modules/ConfigStorage/__test__/ConfigStorage.test.ts
@@ -0,0 +1,21 @@
+import browser from 'webextension-polyfill';
+import { ConfigStorageMigration } from '../ConfigStorage';
+
+import configVersion1 from './config-v1.json';
+import configVersion2 from './config-v2.json';
+
+describe('config migrations', () => {
+	test('migrate config v0-v2', async () => {
+		// Load data
+		localStorage.setItem('config.Main', JSON.stringify(configVersion1));
+
+		// Migrate data
+		const currentVersion = ConfigStorageMigration.version;
+		await ConfigStorageMigration.migrate(0, currentVersion);
+
+		const { appConfig } = await browser.storage.local.get('appConfig');
+
+		expect(appConfig).toEqual(configVersion2);
+		expect(localStorage.getItem('config.Main')).toBeNull();
+	});
+});
