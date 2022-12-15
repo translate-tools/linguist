7dbb617d91c96759424efa3be143133016a5a80e
vitonsky <86191922+vitonsky@users.noreply.github.com>
2022-12-15 23:15:03 +0300

    refactor(#176): update autoTranslation migration

--- src/requests/backend/autoTranslation/migrations.ts
+++ src/requests/backend/autoTranslation/migrations.ts
@@ -2,67 +2,65 @@ import browser from 'webextension-polyfill';
 
 import { SiteData, getPreferences, setPreferences } from './sitePreferences/utils';
 import { addLanguage } from './languagePreferences/utils';
-import { MigrationTask } from '../../../migrations/migrations';
+import { configureMigration } from '../../../migrations/migrations';
 
-export const SitePreferencesMigration: MigrationTask = {
-	version: 1,
-	async migrate(previousVersion) {
-		switch (previousVersion) {
-			case 0: {
-				const allStorageData = await browser.storage.local.get();
-				const storagePrefix = 'SitePreferences:';
-				const prefixLen = storagePrefix.length;
+export const SitePreferencesMigration = configureMigration([
+	{
+		version: 1,
+		/**
+		 * Migrate data from `browser.storage` to a IDB
+		 */
+		async migrate() {
+			// Migrate preferences for sites
+			const sitePreferencesStoragePrefix = 'SitePreferences:';
+			const browserStorage = await browser.storage.local.get();
+			for (const key in browserStorage) {
+				// Skip
+				if (!key.startsWith(sitePreferencesStoragePrefix)) continue;
 
-				// Migrate preferences for sites
-				for (const key in allStorageData) {
-					// Skip
-					if (!key.startsWith(storagePrefix)) continue;
+				// Move item data
+				const entry = browserStorage[key];
+				if (typeof entry === 'object' && 'translateAlways' in entry) {
+					const domain = key.slice(sitePreferencesStoragePrefix.length);
+					if (domain.length > 0) {
+						const currentPreferences = await getPreferences(domain);
+						const preferences: SiteData = currentPreferences || {
+							enableAutoTranslate: false,
+							autoTranslateLanguages: [],
+							autoTranslateIgnoreLanguages: [],
+						};
 
-					// Move item data
-					const entry = allStorageData[key];
-					if (typeof entry === 'object' && 'translateAlways' in entry) {
-						const domain = key.slice(prefixLen);
-						if (domain.length > 0) {
-							// Get current preferences or make new object
-							const preferences: SiteData = (await getPreferences(
-								domain,
-							)) || {
-								enableAutoTranslate: false,
-								autoTranslateLanguages: [],
-								autoTranslateIgnoreLanguages: [],
-							};
-
-							const enableAutoTranslate = !!entry.translateAlways;
-							setPreferences(domain, {
-								...preferences,
-								...{ enableAutoTranslate },
-							});
-						}
+						const enableAutoTranslate = Boolean(entry.translateAlways);
+						setPreferences(domain, {
+							...preferences,
+							...{ enableAutoTranslate },
+						});
 					}
-
-					// Remove item
-					await browser.storage.local.remove(key);
 				}
 
-				// Migrate preferences for languages
-				const languagesKey = 'autoTranslatedLangs';
-				const langsResponse = await browser.storage.local.get(languagesKey);
-				if (languagesKey in langsResponse) {
-					// Migrate data
-					if (Array.isArray(langsResponse[languagesKey])) {
-						for (const lang of langsResponse[languagesKey]) {
-							// Skip invalid types
-							if (typeof lang !== 'string') continue;
-
-							// Add language
-							await addLanguage(lang, true);
-						}
-					}
-
-					// Remove key
-					await browser.storage.local.remove(languagesKey);
-				}
+				// Remove item
+				await browser.storage.local.remove(key);
 			}
-		}
+
+			// Migrate preferences for languages
+			const languagesKey = 'autoTranslatedLangs';
+			const langsResponse = await browser.storage.local.get(languagesKey);
+			if (languagesKey in langsResponse) {
+				// Migrate languages
+				const languages = langsResponse[languagesKey];
+				if (Array.isArray(languages)) {
+					for (const lang of languages) {
+						// Skip invalid types
+						if (typeof lang !== 'string') continue;
+
+						// Add language
+						await addLanguage(lang, true);
+					}
+				}
+
+				// Remove from storage
+				await browser.storage.local.remove(languagesKey);
+			}
+		},
 	},
-};
+]);
