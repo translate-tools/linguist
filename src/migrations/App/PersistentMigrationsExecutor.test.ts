5fb24b04e0a77094efd12b0a480811d3adb35d41
vitonsky <86191922+vitonsky@users.noreply.github.com>
2022-12-17 20:13:36 +0300

    test(#181): share basic tests to external users

--- src/migrations/App/PersistentMigrationsExecutor.test.ts
+++ src/migrations/App/PersistentMigrationsExecutor.test.ts
@@ -1,6 +1,8 @@
-import { clearAllMocks } from '../../lib/tests';
-import { AppMigrationsStorage } from './AppMigrationsStorage';
+import { testPersistentMigrationsExecutor } from '../../lib/migrations/MigrationsExecutor/PersistentMigrationsExecutor.test';
 import { PersistentMigrationsExecutor } from '../../lib/migrations/MigrationsExecutor/PersistentMigrationsExecutor';
+import { clearAllMocks } from '../../lib/tests';
+
+import { AppMigrationsStorage } from './AppMigrationsStorage';
 
 describe('migrations persistence', () => {
 	beforeAll(async () => {
@@ -12,92 +14,8 @@ describe('migrations persistence', () => {
 		await migrationsStorage.prepareStorage();
 	});
 
-	const getMockedMigrations = () => {
-		const migrateFooStub = jest.fn();
-		const migrateBarStub = jest.fn();
-
-		return [
-			{
-				name: 'migrationFoo',
-				migration: {
-					version: 1,
-					migrate: migrateFooStub,
-				},
-			},
-			{
-				name: 'migrationBar',
-				migration: {
-					version: 5,
-					migrate: migrateBarStub,
-				},
-			},
-		];
-	};
-
-	test('migrations does not execute for new users', async () => {
-		const migrationsStorage = new AppMigrationsStorage();
-		const persistentMigrationsExecutor = new PersistentMigrationsExecutor(
-			migrationsStorage,
-		);
-
-		const migrations = getMockedMigrations();
-
-		const migrationFoo = migrations[0];
-		const migrationBar = migrations[1];
-
-		// Call first time and only remember migrations versions
-		await persistentMigrationsExecutor.executeMigrations(migrations);
-		expect(migrationFoo.migration.migrate).not.toHaveBeenCalled();
-		expect(migrationBar.migration.migrate).not.toHaveBeenCalled();
-
-		// Call one more time and do nothing
-		await persistentMigrationsExecutor.executeMigrations(migrations);
-		expect(migrationFoo.migration.migrate).not.toHaveBeenCalled();
-		expect(migrationBar.migration.migrate).not.toHaveBeenCalled();
-	});
-
-	test('migrations run when versions changes', async () => {
-		const migrationsStorage = new AppMigrationsStorage();
-		const persistentMigrationsExecutor = new PersistentMigrationsExecutor(
-			migrationsStorage,
-		);
-
-		const migrations = getMockedMigrations();
-
-		const migrationFoo = migrations[0];
-		const migrationBar = migrations[1];
-
-		// Migrate foo
-		jest.clearAllMocks();
-		migrationFoo.migration.version++;
-
-		await persistentMigrationsExecutor.executeMigrations(migrations);
-		expect(migrationFoo.migration.migrate).toHaveBeenCalled();
-		expect(migrationBar.migration.migrate).not.toHaveBeenCalled();
-
-		// Migrate bar
-		jest.clearAllMocks();
-		migrationBar.migration.version++;
-
-		await persistentMigrationsExecutor.executeMigrations(migrations);
-		expect(migrationFoo.migration.migrate).not.toHaveBeenCalled();
-		expect(migrationBar.migration.migrate).toHaveBeenCalled();
-
-		// Migrate foo and bar
-		jest.clearAllMocks();
-		migrationFoo.migration.version++;
-		migrationBar.migration.version++;
-
-		await persistentMigrationsExecutor.executeMigrations(migrations);
-		expect(migrationFoo.migration.migrate).toHaveBeenCalled();
-		expect(migrationBar.migration.migrate).toHaveBeenCalled();
-
-		// Downgrade bar version
-		jest.clearAllMocks();
-		migrationBar.migration.version = 1;
-
-		await persistentMigrationsExecutor.executeMigrations(migrations);
-		expect(migrationFoo.migration.migrate).not.toHaveBeenCalled();
-		expect(migrationBar.migration.migrate).not.toHaveBeenCalled();
+	testPersistentMigrationsExecutor(() => {
+		const storage = new AppMigrationsStorage();
+		return new PersistentMigrationsExecutor(storage);
 	});
 });
