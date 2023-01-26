0de7c45699c7f7a1134850cd94ebe13d4f617ec5
vitonsky <86191922+vitonsky@users.noreply.github.com>
2023-01-26 12:26:27 +0300

    refactor(#188): bind methods context

--- src/modules/ContentScript/PageTranslationContext.ts
+++ src/modules/ContentScript/PageTranslationContext.ts
@@ -80,6 +80,52 @@ export class PageTranslationContext {
 		await this.startTranslation();
 	}
 
+	// TODO: test the code
+	private async startTranslation() {
+		const $config = this.$config;
+
+		const $selectTranslator = createSelector(
+			$config,
+			(state) => state.selectTranslator,
+			{
+				updateFilter: updateNotEqualFilter,
+			},
+		);
+
+		// TODO: move whole code to a class `SelectTranslatorManager`
+
+		// Make or delete SelectTranslator
+		// We re-create instance to make able a disable select translator
+		// to avoid appending unnecessary nodes to DOM
+		$selectTranslator.watch(this.manageSelectTranslatorInstance.bind(this));
+
+		// Start/stop of SelectTranslator by update config
+		$config
+			.map(({ selectTranslator }) => {
+				return (
+					selectTranslator.enabled &&
+					(!selectTranslator.disableWhileTranslatePage ||
+						// TODO: emit event while start/stop page translator and react on it here
+						!this.pageTranslator.isRun())
+				);
+			})
+			.watch(this.manageSelectTranslatorState.bind(this));
+
+		// Update SelectTranslator
+		$selectTranslator.watch(this.manageSelectTranslatorConfig.bind(this));
+
+		// TODO: move code to class `PageTranslatorManager`
+
+		// Update PageTranslator
+		createSelector($config, (state) => state.pageTranslator, {
+			updateFilter: updateNotEqualFilter,
+		}).watch(this.managePageTranslatorInstance.bind(this));
+
+		// Init page translate
+		// TODO: add option to define stage to detect language and run auto translate
+		runByReadyState(this.onPageLoaded, 'interactive');
+	}
+
 	private manageSelectTranslatorInstance(config: AppConfigType['selectTranslator']) {
 		if (config.enabled) {
 			if (this.selectTranslator !== null) return;
@@ -99,52 +145,6 @@ export class PageTranslationContext {
 
 			this.selectTranslator = null;
 		}
-	}
-
-	// TODO: test the code
-	private async startTranslation() {
-		const $config = this.$config;
-
-		const $selectTranslator = createSelector(
-			$config,
-			(state) => state.selectTranslator,
-			{
-				updateFilter: updateNotEqualFilter,
-			},
-		);
-
-		// TODO: move whole code to a class `SelectTranslatorManager`
-
-		// Make or delete SelectTranslator
-		// We re-create instance to make able a disable select translator
-		// to avoid appending unnecessary nodes to DOM
-		$selectTranslator.watch(this.manageSelectTranslatorInstance);
-
-		// Start/stop of SelectTranslator by update config
-		$config
-			.map(({ selectTranslator }) => {
-				return (
-					selectTranslator.enabled &&
-					(!selectTranslator.disableWhileTranslatePage ||
-						// TODO: emit event while start/stop page translator and react on it here
-						!this.pageTranslator.isRun())
-				);
-			})
-			.watch(this.manageSelectTranslatorState);
-
-		// Update SelectTranslator
-		$selectTranslator.watch(this.manageSelectTranslatorConfig);
-
-		// TODO: move code to class `PageTranslatorManager`
-
-		// Update PageTranslator
-		createSelector($config, (state) => state.pageTranslator, {
-			updateFilter: updateNotEqualFilter,
-		}).watch(this.managePageTranslatorInstance);
-
-		// Init page translate
-		// TODO: add option to define stage to detect language and run auto translate
-		runByReadyState(this.onPageLoaded, 'interactive');
 	}
 
 	private manageSelectTranslatorState(isNeedRunSelectTranslator: boolean) {
@@ -266,6 +266,7 @@ export class PageTranslationContext {
 			if (isNeedAutoTranslate) {
 				const selectTranslator = this.selectTranslator;
 
+				// TODO: eliminate code duplication. Call one method
 				if (
 					selectTranslator !== null &&
 					selectTranslator.isRun() &&
