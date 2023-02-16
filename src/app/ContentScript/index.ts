0a38ad37690277ae750c777a2a664e30a4234086
vitonsky <86191922+vitonsky@users.noreply.github.com>
2023-02-16 12:23:25 +0300

    refactor(#200): move contentscript handlers

--- src/app/ContentScript/index.ts
+++ src/app/ContentScript/index.ts
@@ -1,19 +1,10 @@
 import { getAllTabs } from '../../lib/browser/tabs';
 import { sendTabRequest } from '../../requests/utils';
 
-// Requests
-import { getPageLanguageFactory } from '../../requests/contentscript/getPageLanguage';
-import { getPageTranslateStateFactory } from '../../requests/contentscript/pageTranslation/getPageTranslateState';
-import { pingFactory } from '../../requests/contentscript/ping';
-import { enableTranslatePageFactory } from '../../requests/contentscript/pageTranslation/enableTranslatePage';
-import { disableTranslatePageFactory } from '../../requests/contentscript/pageTranslation/disableTranslatePage';
-import { translateSelectedTextFactory } from '../../requests/contentscript/translateSelectedText';
-
 import { ClientConfig } from './ClientConfig';
 import { PageTranslationContext } from './PageTranslationContext';
 
-// TODO: use builder for this request to ensure types integrity
-// Firstly, we should refactor builder to make it more abstract
+import { requestHandlers } from './requestHandlers';
 
 // TODO: move to common requests
 /**
@@ -53,16 +44,7 @@ export class ContentScript {
 
 		await pageTranslationContext.start();
 
-		const factories = [
-			pingFactory,
-			getPageTranslateStateFactory,
-			getPageLanguageFactory,
-			enableTranslatePageFactory,
-			disableTranslatePageFactory,
-			translateSelectedTextFactory,
-		];
-
-		factories.forEach((factory) => {
+		requestHandlers.forEach((factory) => {
 			factory({
 				$config,
 				pageTranslationContext,
