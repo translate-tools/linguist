107c0f4e6023d8b32373aa85e58dcd0a3419fbde
vitonsky <86191922+vitonsky@users.noreply.github.com>
2023-02-26 12:58:16 +0300

    refactor(#83): move all context menus to a classes

---
+++ src/app/ContextMenus/TranslateSelectionContextMenu.ts
@@ -0,0 +1,34 @@
+import browser, { Menus, Tabs } from 'webextension-polyfill';
+
+import { getMessage } from '../../lib/language';
+import { translateSelectedText } from '../../requests/contentscript/translateSelectedText';
+
+export class TranslateSelectionContextMenu {
+	private menuId = 'translateText';
+	private isEnabled = false;
+
+	public enable() {
+		if (this.isEnabled) return;
+		this.isEnabled = true;
+
+		browser.contextMenus.onClicked.addListener(this.onClicked);
+		browser.contextMenus.create({
+			id: this.menuId,
+			title: getMessage('contextMenu_translateSelectedText'),
+			contexts: ['selection'],
+		});
+	}
+	public disable() {
+		if (!this.isEnabled) return;
+		this.isEnabled = false;
+
+		browser.contextMenus.onClicked.removeListener(this.onClicked);
+		browser.contextMenus.remove(this.menuId);
+	}
+
+	private onClicked = (info: Menus.OnClickData, tab: Tabs.Tab | undefined) => {
+		if (info.menuItemId !== this.menuId || !tab || !tab.id) return;
+
+		translateSelectedText(tab.id);
+	};
+}
--- src/app/index.ts
+++ src/app/index.ts
@@ -3,7 +3,7 @@ import { defaultConfig } from '../config';
 import { AppConfigType } from '../types/runtime';
 import { isBackgroundContext } from '../lib/browser';
 import { AppThemeControl } from '../lib/browser/AppThemeControl';
-import { toggleTranslateItemInContextMenu } from '../lib/browser/toggleTranslateItemInContextMenu';
+import { TranslateSelectionContextMenu } from './ContextMenus/TranslateSelectionContextMenu';
 import { migrateAll } from './migrations/migrationsList';
 import { clearCache } from '../requests/backend/clearCache';
 
@@ -105,12 +105,19 @@ export class App {
 			});
 
 		// Configure context menu
+		const translateSelectionContextMenu = new TranslateSelectionContextMenu();
 		$appConfig
-			.map((config) => config.selectTranslator)
-			.watch((selectTranslator) => {
-				const { enabled, mode } = selectTranslator;
+			.map((config) => {
+				const { enabled, mode } = config.selectTranslator;
 				const isEnabled = enabled && mode === 'contextMenu';
-				toggleTranslateItemInContextMenu(isEnabled);
+				return isEnabled;
+			})
+			.watch((isEnabled) => {
+				if (isEnabled) {
+					translateSelectionContextMenu.enable();
+				} else {
+					translateSelectionContextMenu.disable();
+				}
 			});
 
 		const translatePageContextMenu = new TranslatePageContextMenu();
