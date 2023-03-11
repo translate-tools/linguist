454f5f4cc878eeae71efa215245034044b89be3c
vitonsky <86191922+vitonsky@users.noreply.github.com>
2023-03-11 18:49:29 +0300

    fix(#217): reset TTS to default while remove active TTS

--- src/requests/backend/tts/deleteCustomSpeaker.ts
+++ src/requests/backend/tts/deleteCustomSpeaker.ts
@@ -1,17 +1,27 @@
+import { DEFAULT_TTS } from '../../../config';
 import { buildBackendRequest } from '../../utils/requestBuilder';
 
-// TODO: reset TTS to embedded
 export const [deleteCustomSpeakerFactory, deleteCustomSpeaker] = buildBackendRequest(
 	'tts.deleteCustomSpeaker',
 	{
 		factoryHandler:
 			({ config, backgroundContext }) =>
 				async (id: string) => {
+					const actualConfig = await config.get();
+					const isCurrentModule = actualConfig.ttsModule === id;
+
+					// reset TTS to embedded
+					if (isCurrentModule) {
+						config.set({
+							...actualConfig,
+							ttsModule: DEFAULT_TTS,
+						});
+					}
+
 					const ttsManager = backgroundContext.getTTSManager();
 					await ttsManager.delete(id);
 
-					const { ttsModule } = await config.get();
-					if (ttsModule === id) {
+					if (isCurrentModule) {
 						const ttsController = await backgroundContext.getTTSController();
 						ttsController.updateSpeaker();
 					}
