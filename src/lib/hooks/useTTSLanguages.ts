feb060d3967a48b8105147b522b5a0d05bb9ed23
vitonsky <86191922+vitonsky@users.noreply.github.com>
2023-03-12 11:10:53 +0300

    feat(#217): update TTS languages by config changes

--- src/lib/hooks/useTTSLanguages.ts
+++ src/lib/hooks/useTTSLanguages.ts
@@ -1,13 +1,28 @@
 import { useCallback, useEffect, useState } from 'react';
-import { getTTSLanguages } from '../../requests/backend/tts/getTTSLanguages';
+import { isEqual } from 'lodash';
 
-// TODO: update languages by change TTS module
+import { getTTSLanguages } from '../../requests/backend/tts/getTTSLanguages';
+import { onAppConfigUpdated } from '../../requests/common/appConfigUpdate';
+
 export const useTTSLanguages = () => {
 	const [supportedLanguages, setSupportedLanguages] = useState<string[]>([]);
 
 	// Get languages
 	useEffect(() => {
 		getTTSLanguages().then(setSupportedLanguages);
+	}, []);
+
+	// Update languages by change TTS module
+	useEffect(() => {
+		const cleanup = onAppConfigUpdated(() => {
+			getTTSLanguages().then((langs) => {
+				setSupportedLanguages((value) => {
+					return isEqual(value, langs) ? value : langs;
+				});
+			});
+		});
+
+		return cleanup;
 	}, []);
 
 	const isSupportedLanguage = useCallback(
