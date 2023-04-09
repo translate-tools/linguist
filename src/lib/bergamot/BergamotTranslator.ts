efd615b56dbc0a4e6ee8c2346a7910a837822c8c
vitonsky <86191922+vitonsky@users.noreply.github.com>
2023-04-09 21:20:53 +0300

    refactor(#253): split bergamot frontend code

--- src/lib/bergamot/BergamotTranslator.ts
+++ src/lib/bergamot/BergamotTranslator.ts
@@ -1,5 +1,5 @@
 import { detectLanguage, getMessage } from '../language';
-import { BatchTranslator } from '.';
+import { BatchTranslator } from './frontend';
 
 export class BergamotTranslator {
 	static translatorName = getMessage('common_offlineTranslator', 'Bergamot');
