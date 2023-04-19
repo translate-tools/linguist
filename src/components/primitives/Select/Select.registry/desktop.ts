520b90baa52d07b66bc8828876b994f20a58919b
vitonsky <86191922+vitonsky@users.noreply.github.com>
2023-04-20 00:40:24 +0300

    fix(#294): update types

--- src/components/primitives/Select/Select.registry/desktop.ts
+++ src/components/primitives/Select/Select.registry/desktop.ts
@@ -2,15 +2,19 @@ export * from 'react-elegant-ui/esm/components/Select/Select.registry/desktop';
 
 import { Popup } from 'react-elegant-ui/esm/components/Popup/Popup.bundle/desktop';
 import { cnSelect } from 'react-elegant-ui/esm/components/Select/Select';
-import { regObjects as regObjectsBasic } from 'react-elegant-ui/esm/components/Select/Select.registry/desktop';
+import {
+	ISelectDesktopRegistry,
+	regObjects as regObjectsBasic,
+} from 'react-elegant-ui/esm/components/Select/Select.registry/desktop';
 import { withDefaultProps } from 'react-elegant-ui/esm/hocs/withDefaultProps';
 import { applyMinWidth } from 'react-elegant-ui/esm/hooks/behavior/usePopper/modifiers/applyMinWidth';
 import { Registry } from 'react-elegant-ui/esm/lib/di';
 
 import { applyMaxHeight } from '../applyMaxHeight';
 
-export const regObjects: typeof regObjectsBasic = {
+export const regObjects: ISelectDesktopRegistry = {
 	...regObjectsBasic, // Desktop features
+	// FIXME: remove cast when bug will fixed https://github.com/vitonsky/react-elegant-ui/issues/253
 	PopupComponent: withDefaultProps(Popup, {
 		// at the moment `applyMaxHeight` decrease block size while scroll, it's bug
 		modifiers: [
@@ -19,7 +23,7 @@ export const regObjects: typeof regObjectsBasic = {
 			{ name: applyMaxHeight.name, options: { maxHeight: 200 } },
 		],
 		view: 'default',
-	}),
+	}) as ISelectDesktopRegistry['PopupComponent'],
 };
 
 export const SelectDesktopRegistry = new Registry({ id: cnSelect() }).fill(
