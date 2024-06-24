29afcd36417ef7f5ea68bee166130314628be4c8
Robert Vitonsky <rob@vitonsky.net>
2024-06-25 00:33:07 +0200

    feat: implement language picker and detection

--- site/src/components/i18n.ts
+++ site/src/components/i18n.ts
@@ -1,13 +1,20 @@
 import { initReactI18next } from 'react-i18next';
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import ChainedBackend from 'i18next-chained-backend';
+import resourcesToBackend from 'i18next-resources-to-backend';
 
 // eslint-disable-next-line import/no-named-as-default-member
-i18n.use(initReactI18next) // passes i18n down to react-i18next
+i18n.use(ChainedBackend)
+	.use(LanguageDetector)
+	.use(initReactI18next) // passes i18n down to react-i18next
 	.init({
-		lng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-		// you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-		// if you're using a language detector, do not define the lng option
-
+		fallbackLng: 'en',
+		backend: {
+			backends: [
+				resourcesToBackend((lng) => import(`./Landing/locales/${lng}.json`)),
+			],
+		},
 		interpolation: {
 			escapeValue: false, // react already safes from xss
 		},
