2046fc9e4764f9031b1ee9ba368ddef335b26c79
Robert Vitonsky <rob@vitonsky.net>
2024-06-24 16:47:56 +0200

    feat: track clicks for any links

--- site/src/components/PageLayout/PageLayout.tsx
+++ site/src/components/PageLayout/PageLayout.tsx
@@ -1,19 +1,48 @@
-import React, { FC, PropsWithChildren } from 'react';
+import React, { FC, PropsWithChildren, useEffect } from 'react';
+import Plausible from 'plausible-tracker';
 import { ChakraBaseProvider } from '@chakra-ui/react';
 import Head from '@docusaurus/Head';
 
 import theme from '../theme';
 
 export const PageLayout: FC<PropsWithChildren> = ({ children }) => {
+	useEffect(() => {
+		const plausible = Plausible({
+			domain: 'linguister.io',
+			apiHost: 'https://pulse2.vitonsky.net',
+		});
+
+		plausible.enableAutoPageviews();
+		plausible.enableAutoOutboundTracking();
+
+		// Track clicks
+		document.body.addEventListener('click', (event: MouseEvent) => {
+			// Explore click targets to find a link element
+			const targets = event?.composedPath() || [event.target];
+			for (const target of targets) {
+				if (!(target instanceof HTMLAnchorElement)) continue;
+
+				const timestamp = performance.now();
+				plausible.trackEvent('Link click', {
+					props: {
+						// Current location
+						location: location.toString(),
+						url: target.href,
+						text: target.innerText,
+						// Time since visit page
+						timestamp: timestamp,
+						timestampSeconds: timestamp / 1000,
+					},
+				});
+				break;
+			}
+		});
+	});
+
 	return (
 		<ChakraBaseProvider theme={theme}>
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<script
-					defer
-					data-domain="linguister.io"
-					src="https://pulse2.vitonsky.net/js/script.outbound-links.js"
-				></script>
 				<script
 					async
 					src="https://www.googletagmanager.com/gtag/js?id=G-NGGDLX42RQ"
