23b8b5b3b71f7eae7c1beffa6059c86616a6d7de
Robert Vitonsky <robert@vitonsky.net>
2025-07-07 00:00:31 +0200

    chore: remove legacy locales scripts

--- scripts/locales/sortLocalizationFiles.js
+++
@@ -1,3 +0,0 @@
-const { sortLocalizationFiles } = require('./utils/sortLocalizations');
-
-sortLocalizationFiles();
--- scripts/locales/syncLocalizationsFilesWithSource.js
+++
@@ -1,8 +0,0 @@
-const { syncLocalizationsFilesWithSource } = require('./utils/syncLocalizations');
-const { sortLocalizationFiles } = require('./utils/sortLocalizations');
-
-syncLocalizationsFilesWithSource()
-	.then(sortLocalizationFiles)
-	.then(() => {
-		console.log('Done! Review the result now');
-	});
