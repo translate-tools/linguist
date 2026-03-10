36b6777fdf9a64828498b1a4c8f5b3d7bcbece7c
Robert Vitonsky <robert@vitonsky.net>
2026-03-10 08:52:50 +0100

    feat: build alt links via site origin

--- packages/site/src/plugins/i18n-pages.ts
+++ packages/site/src/plugins/i18n-pages.ts
@@ -1,35 +1,38 @@
 import Mustache from 'mustache';
-import { Plugin } from '@docusaurus/types';
+import { PluginModule } from '@docusaurus/types';
 
 import { LocalesController } from '../i18n/LocalesController';
 
-const getPageUrl = (template: string, locale?: string) => {
+const compilePagePath = (template: string, locale?: string) => {
 	return Mustache.render(template, { locale: locale ?? '' }).replaceAll(/\/{2,}/g, '/');
 };
 
 export default (
-		routes: {
-			url: string;
-			pageComponent: string;
-			i18n: {
-				defaultLocale: string;
-				localesDir: string;
-				namespaces: string[];
-			};
-		}[],
-	) =>
-	(): Plugin => {
+	routes: {
+		url: string;
+		pageComponent: string;
+		i18n: {
+			defaultLocale: string;
+			localesDir: string;
+			namespaces: string[];
+		};
+	}[],
+): PluginModule => {
+	return (context) => {
 		return {
 			name: 'i18n-pages',
 
 			getPathsToWatch() {
-				return routes.map((route) => `${route.i18n.localesDir}/**/*.json`);
+				return [
+					__filename,
+					...routes.map((route) => `${route.i18n.localesDir}/**/*.json`),
+				];
 			},
 
 			async contentLoaded({ actions }) {
 				const { addRoute, createData } = actions;
 
-				for (const { url, pageComponent, i18n } of routes) {
+				for (const { url: urlTemplate, pageComponent, i18n } of routes) {
 					const locales = new LocalesController(i18n.localesDir);
 
 					// Create pages
@@ -40,10 +43,24 @@ export default (
 							language: locale,
 							namespaces: i18n.namespaces,
 							getAltPath(language) {
-								return getPageUrl(
-									url,
+								const pagePath = compilePagePath(
+									urlTemplate,
 									isDefaultLocale ? language : undefined,
 								);
+
+								// Build path
+								const { url, baseUrl } = context.siteConfig;
+
+								const joinedPath = (baseUrl + pagePath).replaceAll(
+									/\/{2,}/g,
+									'/',
+								);
+
+								// Return
+								const isDev = process.env.NODE_ENV === 'development';
+								return isDev
+									? joinedPath
+									: new URL(joinedPath, url).toString();
 							},
 						});
 						const i18nContentPath = await createData(
@@ -52,7 +69,7 @@ export default (
 						);
 
 						addRoute({
-							path: getPageUrl(url, locale),
+							path: compilePagePath(urlTemplate, locale),
 							component: pageComponent,
 							modules: {
 								i18n: i18nContentPath,
@@ -61,7 +78,7 @@ export default (
 
 						if (isDefaultLocale) {
 							addRoute({
-								path: getPageUrl(url),
+								path: compilePagePath(urlTemplate),
 								component: pageComponent,
 								modules: {
 									i18n: i18nContentPath,
@@ -73,3 +90,4 @@ export default (
 			},
 		};
 	};
+};
